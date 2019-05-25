fe6af92a884ebaee324995e642af60ee162a7687
Shruthi1412 <47416139+Shruthi1412@users.noreply.github.com>
2019-05-25 14:17:21 +0530

    Update spinner.php

--- spinner.php
+++ spinner.php
@@ -1,7 +1,7 @@
 <?php
-$host='localhost';
-$username='root';
-$password='';
+$host='arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username='xw52px19w9l8h2de';
+$password='h05d52sz0zr44qgd';
 $db='crud';
 $con= mysqli_connect($host,$username,$password) or die("Connection failed");
 mysqli_select_db($con,$db) or die("db selection failed");
